26a7e9f3e5e3258328f272b2b2de238d2ce5a6f9
AoRyu6 <ryu17615@gmail.com>
2024-09-23 10:13:54 +0900

    feat: コメントモデルの関連付け

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+class CommentsController < ApplicationController
+  def create
+  end
+
+  def destroy
+  end
+end
--- app/models/post.rb
+++ app/models/post.rb
@@ -4,6 +4,7 @@ class Post < ApplicationRecord
   validates :content, presence: true, length: { maximum: 140 }
   belongs_to :user
   has_many :likes, dependent: :destroy
+  has_many :comments, dependent: :destroy
 
   def liked_by?(user)
     likes.exists?(user_id: user.id)
